9050c03fe3b098e7760efdade7324050d6f8d94e
Jaroslav Malý <jarda.maly@gmail.com>
2019-01-17 08:32:36 +0100

    wip

---
+++ app/model/Database/Entity/DocumentType.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types = 1);
+
+namespace App\Model\Database\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\OneToMany;
+use Ramsey\Uuid\UuidInterface;
+
+/**
+ * @ORM\Entity
+ */
+class DocumentType {
+
+	/**
+	 * @var \Ramsey\Uuid\UuidInterface
+	 *
+	 * @ORM\Id
+	 * @ORM\Column(type="uuid", unique=true)
+	 * @ORM\GeneratedValue(strategy="CUSTOM")
+	 * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+	 */
+
+	private $id;
+
+	/**
+	 * @ORM\Column(type="string")
+	 * @var string
+	 */
+	private $name;
+
+	/**
+	 * @OneToMany(targetEntity="Document", mappedBy="type")
+	 * @var \Doctrine\ORM\PersistentCollection
+	 */
+	protected $documents;
+
+
+	public function __construct(string $name) {
+
+		$this->setName($name);
+	}
+
+	public function getId(): UuidInterface {
+		return $this->id;
+	}
+
+	public function getName(): string {
+		return $this->name;
+	}
+
+	/**
+	 * @param string $name
+	 */
+	public function setName(string $name): void {
+		$this->name = $name;
+	}
+
+	/**
+	 * @return \Doctrine\ORM\PersistentCollection
+	 */
+	public function getDocuments(): \Doctrine\ORM\PersistentCollection {
+		return $this->documents;
+	}
+}
